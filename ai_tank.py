7e641eb50f6dde0380f99caf4a465f485aa5723c
lee <30727843+IWantADog@users.noreply.github.com>
2018-01-05 10:40:22 +0800

    modify ai_tank(1.5)

---
+++ ai_tank.py
@@ -0,0 +1,119 @@
+from tank import Tank
+from statemachine import  TankExploring, TankShooting, StateMachine
+from config import  tank_img
+import random
+
+class AiTank(Tank):
+    def __init__(self, aim_tank, aim_boss): #目标坦克坐标，目标boos坐标
+        Tank.__init__(self, tank_img, 2)
+
+        # self.brain = StateMachine()
+        # exploring_state = TankExploring(self)
+        # shooting_state = TankShooting(self)
+        # self.brain.add_state(exploring_state)
+        # self.brain.add_state(shooting_state)
+
+        self.aim_tank = aim_tank
+        self.aim_boss = aim_boss
+        self.state = False
+        self.speed = 1
+        self.last_move_time = 0
+
+    def find_aim(self):   # 返回ai需要移动的方向
+        tank_x, tank_y = self.point.get()
+        aim_tank_x, aim_tank_y = self.aim_tank.get()
+        aim_boss_x, aim_boss_y = self.aim_boss.get()
+
+        self.state = True
+        if -40 <= tank_x - aim_boss_x <= 40:
+            if 0 < aim_boss_y - tank_y <= 150:
+                return 's'
+            elif 0 < tank_y - aim_boss_y <= 150:
+                return 'w'
+
+        if -40 <= tank_y - aim_boss_y <= 40:
+            if 0 < aim_boss_x - tank_x <= 150:
+                return 'd'
+            elif 0 < tank_x - aim_boss_x <= 150:
+                return 'a'
+
+        if -40 <= tank_x - aim_tank_x <= 40:
+            if 0 < aim_tank_y - tank_y <= 150:
+                return 's'
+            elif 0 < tank_y - aim_tank_y <= 150:
+                return 'w'
+
+        if -40 <= tank_y - aim_tank_y <= 40:
+            if 0 < aim_tank_x - tank_x <= 150:
+                return 'd'
+            elif 0 < tank_x - aim_tank_x <= 150:
+                return 'a'
+
+        self.state = False
+        return False
+
+    # def start(self):
+    #     self.brain.set_state('exploring')
+    #
+    # def running(self, current_time):
+    #     self.brain.think(current_time)
+    #     print('state: ', self.brain.active_state.name)
+
+    def ai_move(self, current_time, rate=2000):
+        dire = self.find_aim()
+        if not dire:
+            if current_time > self.last_move_time + rate:
+                dire = random.choice(['w', 's', 'a', 'd'])
+                self.last_move_time = current_time
+            else:
+                dire = self.dirct
+        self.move(dire)
+
+    def ai_shoot(self, current_time, rate):
+        if self.state:
+            return self.shoot(current_time=current_time, rate=rate)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,94 @@
 import pygame
+from pygame.locals import *
+from pygame.sprite import Group
+from tank import Tank
+from ai_tank import AiTank
+from point import Point
+import sys
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((500, 500))
     pygame.display.set_caption('Tank Wars')
-    tank = pygame.image.load('ttank.png').convert()
 
-    while 1:
+    framerate = pygame.time.Clock()
 
-        screen.blit(tank, (100, 100))
+    #boss
+    boss_point = Point(300, 300)
+
+    #player
+    tt = Tank('tank.png', 2)
+
+    #player bullent group
+    bullet_group = Group()
+
+    #ai_tank
+    ai1 = AiTank(tt.point, boss_point)
+    # ai1.start()
+
+    #ai_tank bullent group
+    ai_bullet_group = Group()
+
+
+
+    #tank group
+    group = Group()
+    group.add(tt)
+    group.add(ai1)
+
+
+    while True:
+        framerate.tick(30)
+        current_time = pygame.time.get_ticks()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        keys = pygame.key.get_pressed()
+        if keys[K_w]:
+            tt.move('w')
+        elif keys[K_s]:
+            tt.move('s')
+        elif keys[K_a]:
+            tt.move('a')
+        elif keys[K_d]:
+            tt.move('d')
+
+        if keys[K_j]:
+            new_bullet = tt.shoot(current_time, 300)
+            if new_bullet:
+                bullet_group.add(new_bullet)
+            # pass
+
+        for bullet in bullet_group.sprites():
+            if not bullet.islive:
+                bullet_group.remove(bullet)
+                # passj
+
+        # ai1.running(current_time)
+        ai1.ai_move(current_time, 3000)
+        ai_tank_bullet = ai1.ai_shoot(current_time, 300)
+        if ai_tank_bullet:
+            ai_bullet_group.add(ai_tank_bullet)
+
+        for bullet in ai_bullet_group.sprites():
+            if not bullet.islive:
+                ai_bullet_group.remove(bullet)
+                # passj
+
+        # print('1111 ', len(bullet_group.sprites()))
+        screen.fill((0,0,0))
+
+        group.update(current_time)
+        group.draw(screen)
+
+
+        bullet_group.update()
+        ai_bullet_group.update()
+
+        bullet_group.draw(screen)
+        ai_bullet_group.draw(screen)
+
+
         pygame.display.update()
