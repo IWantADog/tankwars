a980da4bed3754d2ae1d1b1efe202441e5f60e47
benjilee <lsdk5453@gmail.com>
2020-06-22 14:52:46 +0800

    [MODIFY]refactor tank and robot tank class

--- tank.py
+++ tank.py
@@ -1,26 +1,23 @@
+import sys
+
 import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite, Group
 from point import Point
-from bullet import Bullet, load_bullet_images
+from bullet import Bullet
 from wall import Brickwall
 from collide import player_ai_collide, tank_wall_collide
-import sys
-from config import img_width, img_height, bullet_height, bullet_width,\
-    birth_img, boom_img, windows_height, windows_width, area_height, area_width\
-    , default_point, player_tank_path, ai_tank_path, bullet_path
+
+from config import *
 
 class Tank(Sprite):
-    def __init__(self, images_list, columns=2, point=default_point):
+    master_image = None
+
+    def __init__(self, columns=2, point=default_point):
         Sprite.__init__(self)
         self.frame = 2
         self.last_time = 0
-        self.master_image = images_list
-        self.width, self.height = images_list[0].get_size()
-        self.birth_image = pygame.image.load(birth_img).convert_alpha()
-        self.boom_image = pygame.image.load(boom_img).convert_alpha()
-        self.frame_width = self.birth_image.get_width() // 4
-        self.frame_height = self.birth_image.get_height()
+        
         self.columns = columns
         self.first_frame = 2
         self.last_frame = self.first_frame + columns - 1
@@ -34,10 +31,30 @@ class Tank(Sprite):
         self.old_dirct = -1                  # 上一次的方向
         self.point = Point(point)                   # 位置
         self.speed = 3.0                     # 速度
-        self.rect = Rect(self.point.x, self.point.y, self.frame_width, self.frame_height)
         self.collide_dirct = {'w': False, 's': False, 'a': False, 'd': False}  # 碰撞方向
         #bullet
         self.last_shoot_time = 0
+    
+    def load_images(self, images=None, birth_imgs=None, boom_imgs=None):
+        if images is None:
+            images = player_tank_path
+
+        if birth_imgs is None:
+            birth_imgs = birth_img
+
+        if boom_imgs is None:
+            boom_imgs = boom_img
+
+        # TODO 减少图片的加入数量，所有的图片仅载入一次
+        self.master_image = [pygame.image.load(item).convert_alpha() for item in images]
+
+        self.width, self.height = self.master_image[0].get_size()
+        self.birth_image = pygame.image.load(birth_imgs).convert_alpha()
+        self.boom_image = pygame.image.load(boom_imgs).convert_alpha()
+        self.frame_width = self.birth_image.get_width() // 4
+        self.frame_height = self.birth_image.get_height()
+        self.rect = Rect(self.point.x, self.point.y, self.frame_width, self.frame_height)
+
 
     def get_point(self):
         return self.point
@@ -61,7 +78,7 @@ class Tank(Sprite):
     def get_birth(self):
         return self.birth
 
-    def move(self, dire, group): #direction
+    def move(self, dire, group=None): #direction
         if self.life > 0:
             self.ismoved = True
             self.dirct = dire
@@ -125,9 +142,10 @@ class Tank(Sprite):
 
             self.collide_dirct = {'w': False, 's': False, 'a': False, 'd': False}
 
-    def shoot(self, current_time, bullet_images, rate=30):
+    def shoot(self, current_time, rate=30):
         if current_time > self.last_shoot_time + rate and self.life > 0:
-            bullet = Bullet(bullet_images)
+            bullet = Bullet()
+            bullet.load_images()
             bullet.choose_dire(self.dirct)
             x, y = self.point.get()
             dis_x = (self.frame_width/2) - (bullet.width/2)
@@ -137,7 +155,6 @@ class Tank(Sprite):
             bullet.lanch((x, y))
             self.last_shoot_time = current_time
             return bullet
-
 
     def update(self, current_time, rate=30):
         if self.life > 0 :
@@ -168,107 +185,20 @@ class Tank(Sprite):
             self.boom -= 1
 
 
-def load_images(images_list):
-    return [pygame.image.load(item).convert_alpha() for item in images_list]
-
-
-if __name__ == '__main__':
-    pygame.init()
-    pygame.mixer.init()
-
-    start_music = pygame.mixer.Sound('music\\start.ogg')
-    channel = pygame.mixer.find_channel(True)
-    channel.set_volume(0.5)
-    channel.play(start_music)
-
-    screen = pygame.display.set_mode((900, 600))
-    pygame.display.set_caption('Tank Wars')
-
-    framerate = pygame.time.Clock()
-
-    #player
-    p_tank = load_images(player_tank_path)
-    tt = Tank(p_tank, 2)
-    group = Group()
-    group.add(tt)
-
-    #ai tank
-    # ai_tank = load_images(ai_tank_path)
-    # ai_t = AiTank(ai_tank)
-    # ai_group = Group()
-    # ai_group.add(ai_t)
-
-    #player bullent
-    bullet_group = Group()
-
-    #ai bullet
-    # ai_bullet_group = Group()
-
-    #wall group
-    wall_group = Group()
-    #brickwall
-    bwall = Brickwall((300, 100))
-
-    wall_group.add(bwall)
-
-    bullet_images = load_images(bullet_path)
-
-    while True:
-        framerate.tick(30)
-        current_time = pygame.time.get_ticks()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        keys = pygame.key.get_pressed()
+class PlayerTank(Tank):
+    def controller(self, keys, current_time):
         if keys[K_w]:
-            tt.move('w')
+            self.move('w')
         elif keys[K_s]:
-            tt.move('s')
+            self.move('s')
         elif keys[K_a]:
-            tt.move('a')
+            self.move('a')
         elif keys[K_d]:
-            tt.move('d')
+            self.move('d')
+        else:
+            pass
 
         if keys[K_j]:
-            new_bullet = tt.shoot(current_time,bullet_images, 300)
-            if new_bullet:
-                bullet_group.add(new_bullet)
-            # pass
+            return self.shoot(current_time, 300)
 
-        for bullet in bullet_group.sprites():
-            # print(bullet.point, bullet.start_point, bullet.point.get_distance(bullet.start_point))
-            if not bullet.islive:
-                bullet_group.remove(bullet)
-                # passj
-        # for ai in ai_group.sprites():
-        #     ai.ai_move(current_time, p_tank.get_point(), p_tank.get_point(), rate=3000)
-        #     ai_tank_bullet = ai.ai_shoot(current_time, 300)
-        #     if ai_tank_bullet:
-        #         ai_bullet_group.add(ai_tank_bullet)
-        #
-        # for bullet in ai_bullet_group.sprites():
-        #     if not bullet.islive:
-        #         ai_bullet_group.remove(bullet)
-
-        # print('1111 ', len(bullet_group.sprites()))
-        screen.fill((0,0,0))
-
-        group.update(current_time)
-        group.draw(screen)
-
-        # ai_group.update(current_time)
-        # ai_group.draw(screen)
-
-        bullet_group.update()
-        bullet_group.draw(screen)
-
-        # ai_bullet_group.update()
-        # ai_bullet_group.draw(screen)
-
-        #wall_group
-        wall_group.update()
-        wall_group.draw(screen)
-
-        pygame.display.update()
+        return None
